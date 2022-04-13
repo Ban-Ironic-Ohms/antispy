3e9cf28b72fd2839913004f6c92f4b504aacf881
Ban-Ironic-Ohms <micahrobinson93@gmail.com>
2022-04-13 13:41:49 -0700

    borked go guardian

--- v2/antispy2.bat
+++ v2/antispy2.bat
@@ -2,11 +2,11 @@
 
 :loop
 
-tasklist /fi "ImageName eq Chromium.Goguardian.GGWin" /fo csv 2>NUL | find /I "Chromium.Goguardian.GGWin">NUL
+tasklist /fi "ImageName eq Chromium.Goguardian.GGWindowsHost.exe" /fo csv 2>NUL | find /I "Chromium.Goguardian.GGWindowsHost.exe">NUL
 if "%ERRORLEVEL%"=="0" goto :kill
 timeout 1
 goto :loop
 
 :kill
-TASKKILL /F /IM "ClassroomWindows.exe"
+TASKKILL /F /IM "Chromium.Goguardian.GGWindowsHost.exe"
 goto :loop
